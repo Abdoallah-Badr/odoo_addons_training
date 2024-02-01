1543a279cbf9021cec9ade1d98ae969107666afc
HossamAE <HossamA.Eissa@gmail.com>
2024-02-01 13:59:56 +0200

    first commit with initial files

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Real Estate",
+    'description': "task of odoo17 documentation",
+    'author': "Upward - H",
+    'website': "",
+    'version': "17.0.1.0",
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'application': True,
+    'category': 'Uncategorized',
+}
