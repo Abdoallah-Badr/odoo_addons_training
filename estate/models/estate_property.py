57c354948792ed2b474d4b6e2e4ea9df2b013475
mohsen <amohsenmohamed571@gmail.com>
2024-02-06 13:03:24 +0200

    add buttons in property two determine the status of property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api, _
+from odoo import models, fields, api, _, exceptions
 from datetime import date, datetime, timedelta
 from odoo.exceptions import ValidationError
 
@@ -8,6 +8,7 @@ class EstateProperty(models.Model):
     _description = 'EstatePropertyDescription'
 
     name = fields.Char(string='Title', requried=1, size=20)
+    status = fields.Selection([('canceled', 'Canceled'), ('new', 'New'), ('sold', 'Sold')], string='Statsu')
     property_tag_id = fields.Many2many('estate.property.tags', string="Property Type")
     property_type_id = fields.Many2one('estate.property.type', string="Property Type")
     postcode = fields.Char(string='Postcode', size=20)
@@ -29,17 +30,15 @@ class EstateProperty(models.Model):
         ('south', 'South'),
         ('east', 'East'),
         ('west', 'West'),
-    ],string='Garden Orientation')
-    gardens_area=fields.Integer(string="Garden Area",default=0)
+    ], string='Garden Orientation')
+    gardens_area = fields.Integer(string="Garden Area", default=0)
     total_area = fields.Integer(string='Total Area', compute='_compute_total_area')
     offer_ids = fields.One2many('estate.property.offer', 'property_id')
-    best_price = fields.Float(string='Best Price', compute='_compute_best_price', store=True,default=0)
-
+    best_price = fields.Float(string='Best Price', compute='_compute_best_price', store=True, default=0)
 
     def _compute_total_area(self):
         for rec in self:
-            rec.total_area = rec.living_area + rec.garden_area
-
+            rec.total_area = rec.living_area + rec.gardens_area
 
     @api.depends('price')
     def _compute_best_price(self):
@@ -51,15 +50,38 @@ class EstateProperty(models.Model):
         for rec in self:
             # print(rec.offer_ids.mapped('price'))
             # print(max(rec.offer_ids.mapped('price')))
-            rec.best_price = max(rec.offer_ids.mapped('price'),default=0)
-
+            rec.best_price = max(rec.offer_ids.mapped('price'), default=0)
 
     @api.onchange('garden')
     def garden_area(self):
         for rec in self:
             if rec.garden:
-                rec.gardens_area=10
-                rec.garden_orientation='north'
+                rec.gardens_area = 10
+                rec.garden_orientation = 'north'
             else:
-                rec.gardens_area=0
-                rec.garden_orientation=''
+                rec.gardens_area = 0
+                rec.garden_orientation = ''
+
+    def action_do_something(self):
+        for record in self:
+            record.name = "Something"
+        return True
+
+    def cancel_property(self):
+        for rec in self:
+            rec.status = 'canceled'
+        return {
+            'effect': {
+                'fadeout': 'slow',
+                'message': 'Click sucess',
+                'type': 'rainbow_man'
+            }
+        }
+
+
+    def confirm_sale(self):
+        for record in self:
+            if record.status == 'canceled':
+                raise exceptions.UserError("Cannot confirm sale for a canceled item.")
+            else:
+                record.status='sold'
