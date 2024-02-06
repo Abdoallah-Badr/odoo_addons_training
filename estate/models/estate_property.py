5161e11b55ff01c81c1cc969f2f3500cb3f70b2d
mohsen <amohsenmohamed571@gmail.com>
2024-02-06 09:47:52 +0200

    Revert "Compute a validity date for offers."
    This reverts commit 216b0d18ead77bc0ef3d1f0abe88b3292e19438b.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -41,6 +41,10 @@ class EstateProperty(models.Model):
             rec.total_area = rec.living_area + rec.garden_area
 
 
+    # @api.depends('offer_ids.price')
+    # def _compute_best_price(self):
+    #     for property_record in self:
+    #         property_record.best_price = max(property_record.offer_ids.mapped('price'), default=0.0)
 
     @api.depends('offer_ids')
     def _compute_best_price(self):
@@ -48,5 +52,3 @@ class EstateProperty(models.Model):
             # print(rec.offer_ids.mapped('price'))
             # print(max(rec.offer_ids.mapped('price')))
             rec.best_price = max(rec.offer_ids.mapped('price'),default=0)
-
-
