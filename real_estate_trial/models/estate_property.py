cadf15683afaf9904c895b18447daa4e47fab304
Abdoallah-Badr <abdoallahbadr@outlook.com>
2024-02-08 15:06:25 +0200

    add best_offer field and validity date functionality

--- real_estate_trial/models/estate_property.py
+++ real_estate_trial/models/estate_property.py
@@ -1,37 +1,63 @@
-from odoo import fields,models,api
+from odoo import fields, models, api
+
 
 class Property(models.Model):
-    _name='estate.property'
-    _description='property model for estate module'
+    _name = 'estate.property'
+    _description = 'property model for estate module'
 
-    title= fields.Char(string='Title',required=True)
-    description = fields.Text(string='Description',required=True)
-    post_code=fields.Integer(string='Postcode')
-    expected_price=fields.Float(string='Expected price')
-    bedrooms=fields.Integer(string='Bedrooms',default=2 ,required=True)
-    facades=fields.Integer(string='Facades')
-    garden=fields.Boolean(string='Garden')
-    garden_orien=fields.Selection([('west',"West"),('east','East'),('north','North'),('south',"South")])
-    active=fields.Boolean(string='Active')
-    available_form=fields.Date(string='Available from',copy=False,default=fields.Datetime.now)
-    sell_price=fields.Float('Selling price',copy=False)
-    garage=fields.Boolean(string='Garage')
-    status = fields.Selection([('new','New'),('received','Received'),('accepted','Accepted'),('sold','Sold'),('canceled','Canceled')],required=True)
-    # area fileds
+    title = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Description', required=True)
+    post_code = fields.Integer(string='Postcode')
+    bedrooms = fields.Integer(string='Bedrooms', default=2, required=True)
+    facades = fields.Integer(string='Facades')
+    garden = fields.Boolean(string='Garden')
+
+    garden_orien = fields.Selection([('west', "West"), ('east', 'East'), ('north', 'North'), ('south', "South")],compute='_compute_graden_defaults', readonly=False)
+    status = fields.Selection([('new', 'New'), ('received', 'Received'), ('accepted', 'Accepted'), ('sold', 'Sold'),('canceled', 'Canceled')], required=True)
+
+    active = fields.Boolean(string='Active')
+    available_form = fields.Date(string='Available from', copy=False, default=fields.Datetime.now)
+
+    #price fields
+    expected_price = fields.Float(string='Expected price')
+    best_offer = fields.Float(string='Best offer',compute='_compute_best_offer')
+    sell_price = fields.Float('Selling price', copy=False)
+
+    garage = fields.Boolean(string='Garage')
+
+    # area fields
     living_area = fields.Integer(string='Living area (sqm)')
-    garden_area = fields.Integer(string='Garden area (sqm)')
-    total_area=fields.Integer(compute='_compute_total_area',string='Total area (sqm)',readonly=True)
+    garden_area = fields.Integer(string='Garden area (sqm)', compute='_compute_graden_defaults', readonly=False)
+    total_area = fields.Integer(compute='_compute_total_area', string='Total area (sqm)', readonly=True)
 
 
     # related fields
-    property_type_id= fields.Many2one('estate.property.type')
+    property_type_id = fields.Many2one('estate.property.type')
     property_tag_ids = fields.Many2many('estate.property.tag')
-    offer_ids=fields.One2many('estate.property.offer','property_id',string=' ')
-    salesman= fields.Many2one('res.users', string='Salesperson', index=True, tracking=True, default=lambda self: self.env.user)
-    buyer= fields.Many2one('res.partner',string="Buyer",copy=False)
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string=' ')
+    salesman = fields.Many2one('res.users', string='Salesperson', index=True, tracking=True,
+                               default=lambda self: self.env.user)
+    buyer = fields.Many2one('res.partner', string="Buyer", copy=False)
 
-    @api.onchange('living_area','garden_area')
+    @api.onchange('living_area', 'garden_area')
     def _compute_total_area(self):
         for rec in self:
             rec.total_area = rec.living_area + rec.garden_area
 
+    @api.onchange('garden')
+    def _compute_graden_defaults(self):
+        if self.garden == True:
+            self.garden_area = 10
+            self.garden_orien = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orien = ''
+
+    @api.onchange('offer_ids')
+    def _compute_best_offer(self):
+        price_list = []
+        for rec in self.offer_ids:
+            price_list.append(rec.price)
+
+        if len(price_list) != 0:
+            self.best_offer = max(price_list)
--- real_estate_trial/models/estate_property_offer.py
+++ real_estate_trial/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models,fields
+from odoo import models,fields,api
+import datetime
 class Property_offer(models.Model):
     _name = 'estate.property.offer'
     _description = 'property offer model for estate module'
@@ -7,6 +8,9 @@ class Property_offer(models.Model):
     status = fields.Selection([('accepted','Accepted'),('refused','Refused')],string='Status',copy=False)
     partner_id = fields.Many2one('res.partner',string="Partner",required=True)
     property_id=fields.Many2one('estate.property',required=True)
+    validity = fields.Integer(string='Validity',default=7)
+    deadline = fields.Date(string='Deadline')
 
-
-
+    @api.onchange('validity')
+    def _compute_deadline(self):
+        self.deadline = datetime.date.today()+ datetime.timedelta(days=self.validity)
