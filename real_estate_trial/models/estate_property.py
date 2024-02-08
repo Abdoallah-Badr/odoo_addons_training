063f56be5faf9571dc0dad73cf642dd915f117a5
Abdoallah-Badr <abdoallahbadr@outlook.com>
2024-02-08 11:46:50 +0200

    add compute method total_area in offer model

--- real_estate_trial/models/estate_property.py
+++ real_estate_trial/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields,models
+from odoo import fields,models,api
 
 class Property(models.Model):
     _name='estate.property'
@@ -15,10 +15,13 @@ class Property(models.Model):
     active=fields.Boolean(string='Active')
     available_form=fields.Date(string='Available from',copy=False,default=fields.Datetime.now)
     sell_price=fields.Float('Selling price',copy=False)
-    living_area = fields.Integer(string='Living area (sqm)')
     garage=fields.Boolean(string='Garage')
-    garden_area = fields.Integer(string='Garden area (sqm)')
     status = fields.Selection([('new','New'),('received','Received'),('accepted','Accepted'),('sold','Sold'),('canceled','Canceled')],required=True)
+    # area fileds
+    living_area = fields.Integer(string='Living area (sqm)')
+    garden_area = fields.Integer(string='Garden area (sqm)')
+    total_area=fields.Integer(compute='_compute_total_area',string='Total area (sqm)',readonly=True)
+
 
     # related fields
     property_type_id= fields.Many2one('estate.property.type')
@@ -26,3 +29,9 @@ class Property(models.Model):
     offer_ids=fields.One2many('estate.property.offer','property_id',string=' ')
     salesman= fields.Many2one('res.users', string='Salesperson', index=True, tracking=True, default=lambda self: self.env.user)
     buyer= fields.Many2one('res.partner',string="Buyer",copy=False)
+
+    @api.onchange('living_area','garden_area')
+    def _compute_total_area(self):
+        for rec in self:
+            rec.total_area = rec.living_area + rec.garden_area
+
