32291bfffe0bb85a988151cf1defe9ed3b0b0e8e
Abdoallah-Badr <abdoallahbadr@outlook.com>
2024-02-12 17:34:45 +0200

    finish day 10 in docs

--- real_estate_trial/models/estate_property.py
+++ real_estate_trial/models/estate_property.py
@@ -1,11 +1,11 @@
-from odoo import fields, models, api,_
+from odoo import fields, models, api, _
 from odoo.exceptions import UserError
-from num2words import num2words
+
 
 class Property(models.Model):
     _name = 'estate.property'
     _description = 'property model for estate module'
-    _rec_name="title"
+    _rec_name = "title"
 
     title = fields.Char(string='Title', required=True)
     description = fields.Text(string='Description', required=True)
@@ -16,40 +16,44 @@ class Property(models.Model):
     garage = fields.Boolean(string='Garage')
 
     garden_orien = fields.Selection([('west', "West"), ('east', 'East'), ('north', 'North'), ('south', "South")],
-                                    compute='_compute_graden_defaults', readonly=False)
-    status = fields.Selection([('new', 'New'), ('received', 'Received'), ('accepted', 'Accepted'), ('sold', 'Sold'),
-                               ('canceled', 'Canceled')], required=True)
+                                    readonly=False)
+    status = fields.Selection(
+        [('new', 'New'), ('received', 'Received'), ('accepted', 'Accepted'), ('accepted', 'Accepted'), ('sold', 'Sold'),
+         ('canceled', 'Canceled')], required=True)
 
     active = fields.Boolean(string='Active')
     available_form = fields.Date(string='Available from', copy=False, default=fields.Datetime.now)
 
     # price fields
     expected_price = fields.Float(string='Expected price')
-    best_offer = fields.Float(string='Best offer', compute='_compute_best_offer' ,default=0)
-    sell_price = fields.Float('Selling price', copy=False)
-    sell_price_inwords= fields.Char('price in words',compute='_compute_amount_to_words')
+    best_offer = fields.Float(string='Best offer', default=0, compute='_compute_best_offer')
+    sell_price = fields.Float('Selling price', copy=False,
+                              # compute='_compute_selling_price',
+                              store=True)
+    sell_price_inwords = fields.Char('price in words', compute='_compute_amount_to_words')
 
     # area fields
     living_area = fields.Integer(string='Living area (sqm)')
-    garden_area = fields.Integer(string='Garden area (sqm)', compute='_compute_graden_defaults', readonly=False)
-    total_area = fields.Integer(compute='_compute_total_area', string='Total area (sqm)', readonly=True)
+    garden_area = fields.Integer(string='Garden area (sqm)', readonly=False)
+    total_area = fields.Integer(compute='_compute_total_area', string='Total area (sqm)', readonly=True, store=True)
 
     # related fields
     property_type_id = fields.Many2one('estate.property.type')
     property_tag_ids = fields.Many2many('estate.property.tag')
-    offer_ids = fields.One2many('estate.property.offer', 'property_id', string=' ')
-    salesman = fields.Many2one('res.users', string='Salesperson', index=True, tracking=True,default=lambda self: self.env.user)
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', store=True)
+    salesman = fields.Many2one('res.users', string='Salesperson', index=True, tracking=True,
+                               default=lambda self: self.env.user)
 
     buyer = fields.Many2one('res.partner', string="Buyer", copy=False)
     state = fields.Selection([('sold', 'Sold'), ('cancel', 'Cancel')])
 
-    @api.onchange('living_area', 'garden_area')
+    @api.depends('living_area', 'garden_area')
     def _compute_total_area(self):
         for rec in self:
             rec.total_area = rec.living_area + rec.garden_area
 
     @api.onchange('garden')
-    def _compute_graden_defaults(self):
+    def _compute_garden_defaults(self):
         if self.garden == True:
             self.garden_area = 10
             self.garden_orien = 'north'
@@ -58,6 +62,15 @@ class Property(models.Model):
             self.garden_orien = ''
 
     @api.onchange('offer_ids')
+    def _compute_selling_price(self):
+        for offer in self.offer_ids:
+            print(offer)
+            if offer.status == 'accepted':
+                # accepeted_list.append(offer)
+                self.sell_price = offer.price
+                self.buyer = offer.partner_id
+
+    @api.depends('offer_ids')
     def _compute_best_offer(self):
         price_list = []
         for rec in self.offer_ids:
@@ -67,11 +80,14 @@ class Property(models.Model):
             self.best_offer = max(price_list)
         else:
             self.best_offer = 0
+
+
     @api.onchange('sell_price')
     def _compute_amount_to_words(self):
-        res_currency=self.env.user.company_id.currency_id
+        res_currency = self.env.user.company_id.currency_id
         for rec in self:
-            rec.sell_price_inwords=res_currency.amount_to_text(rec.sell_price)
+            rec.sell_price_inwords = res_currency.amount_to_text(rec.sell_price)
+
 
     def action_property_sold(self):
         for rec in self:
@@ -85,8 +101,9 @@ class Property(models.Model):
 
     def action_property_cancel(self):
         for rec in self:
-            rec.state = 'cancel'
-            rec.status = 'canceled'
-            return
-
-
+            if rec.state == 'sold':
+                raise UserError("sold properties can't be canceled")
+            else:
+                rec.state = 'cancel'
+                rec.status = 'canceled'
+                return
--- real_estate_trial/models/estate_property_offer.py
+++ real_estate_trial/models/estate_property_offer.py
@@ -1,16 +1,36 @@
-from odoo import models,fields,api
+from odoo import models, fields, api
+from odoo.exceptions import UserError
 import datetime
+
+
 class Property_offer(models.Model):
     _name = 'estate.property.offer'
     _description = 'property offer model for estate module'
 
     price = fields.Float('Price')
-    status = fields.Selection([('accepted','Accepted'),('refused','Refused')],string='Status',copy=False)
-    partner_id = fields.Many2one('res.partner',string="Partner",required=True)
-    property_id=fields.Many2one('estate.property',required=True)
-    validity = fields.Integer(string='Validity',default=7)
+    status = fields.Selection([('accepted', 'Accepted'), ('refused', 'Refused')], string='Status', copy=False)
+    partner_id = fields.Many2one('res.partner', string="Partner", required=True)
+    property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer(string='Validity', default=7)
     deadline = fields.Date(string='Deadline')
 
-    @api.onchange('validity')
+    @api.depends('validity')
     def _compute_deadline(self):
-        self.deadline = datetime.date.today()+ datetime.timedelta(days=self.validity)
+        self.deadline = datetime.date.today() + datetime.timedelta(days=self.validity)
+
+    def action_property_accept(self):
+        for rec in self:
+            if rec.status == 'refused':
+                raise UserError('this offer refused before')
+            else:
+                rec.status = 'accepted'
+                return
+
+
+    def action_property_refuse(self):
+        for rec in self:
+            if rec.status == 'accepted':
+                raise UserError('this offer accepted before')
+            else:
+                rec.status = 'refused'
+                return
